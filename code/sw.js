84d9bab5895513c6607d279ce2e7fddee7a9d4e1
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2022-06-17 14:00:45 +0100

    WPA

--- code/sw.js
+++ code/sw.js
@@ -51,6 +51,19 @@ self.addEventListener('install', function(event){
 self.addEventListener('activate', e =>{
     self.clients.claim()
 })
+
+buttonInstall.addEventListener('click', async () => {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+  });
 /*
 self.addEventListener('fetch', async e =>{
     const req = e.request
